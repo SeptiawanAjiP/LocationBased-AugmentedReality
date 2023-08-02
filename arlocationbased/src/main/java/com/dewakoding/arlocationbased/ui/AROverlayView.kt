a132bd42f8ec428e20b7a55f13350aa63e51a6ba
halodewakoding <halodewakoding@gmail.com>
2023-08-02 09:11:14 +0700

    feat(ui): add added cardview as information point

--- arlocationbased/src/main/java/com/dewakoding/arlocationbased/ui/AROverlayView.kt
+++ arlocationbased/src/main/java/com/dewakoding/arlocationbased/ui/AROverlayView.kt
@@ -2,53 +2,29 @@ package com.dewakoding.arlocationbased.ui
 
 import android.app.Activity
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Typeface
 import android.hardware.SensorManager
 import android.location.Location
 import android.opengl.Matrix
-import android.view.MotionEvent
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.core.content.res.ResourcesCompat
+import android.widget.ImageView
+import android.widget.TextView
 import com.dewakoding.arlocationbased.R
 import com.dewakoding.arlocationbased.helper.LocationHelper.ECEFtoENU
 import com.dewakoding.arlocationbased.helper.LocationHelper.WSG84toECEF
 import com.dewakoding.arlocationbased.listener.PointClickListener
 import com.dewakoding.arlocationbased.model.ARPoint
-import kotlin.math.roundToInt
 
-class AROverlayView constructor(activity: Activity, val arPoints: MutableList<ARPoint>?, val icon: Int = R.drawable.house, val pointClickListener: PointClickListener):
+class AROverlayView constructor(activity: Activity, val arPoints: MutableList<ARPoint>?, val icon: Int = R.drawable.place, val pointClickListener: PointClickListener):
     View(activity) {
 
     private var projectionMatrix = FloatArray(16)
     private var currentLocation: Location? = null
     private val arFrameLayout: FrameLayout = activity.findViewById(R.id.ar_frame_layout)
-    val pointSize = 70
-
-    private val onPointClickListener = OnTouchListener { _, event ->
-        event?.let { safeEvent ->
-            when (safeEvent.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    performClick()
-                    val x = safeEvent.x
-                    val y = safeEvent.y
-
-                    arPoints?.forEach {
-                        if (x >= it.x!! - pointSize && x <= it.x!! + pointSize
-                            && y >= it.y!! - pointSize && y <= it.y!! + pointSize) {
-                            pointClickListener.onClick(it)
-                        }
-                    }
-                }
-                else -> {}
-            }
-        }
-
-        false
-    }
+    private val arPointLayouts: MutableList<View> = mutableListOf()
 
     fun start() {
         if (parent != null) {
@@ -56,7 +32,6 @@ class AROverlayView constructor(activity: Activity, val arPoints: MutableList<AR
             viewGroup.removeView(this)
         }
         arFrameLayout.addView(this)
-        setOnTouchListener(onPointClickListener)
     }
 
     fun updateProjectionMatrix(matrix: FloatArray) {
@@ -91,18 +66,11 @@ class AROverlayView constructor(activity: Activity, val arPoints: MutableList<AR
         this.currentLocation = currentLocation
         invalidate()
     }
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         if (currentLocation == null) {
             return
         }
-
-        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-        paint.style = Paint.Style.FILL
-        paint.color = Color.WHITE
-        paint.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
-        paint.textSize = 60f
 
         arPoints?.let {
             for (i in this.arPoints!!.indices) {
@@ -129,20 +97,65 @@ class AROverlayView constructor(activity: Activity, val arPoints: MutableList<AR
                     arPoints[i].x = x
                     arPoints[i].y = y
 
-                    canvas.drawCircle(x, y, pointSize.toFloat(), paint)
-                    canvas.drawText(arPoints.get(i).name, x - (30 * arPoints.get(i).name.length / 2), y - 80, paint);
+                    if (arPointLayouts.size <= i) {
+                        // Create a new AR point layout
+                        val arPointCardView = LayoutInflater.from(context).inflate(R.layout.cardview_point, null)
+                        val arPointIcon = arPointCardView.findViewById<ImageView>(R.id.img_status)
+                        val arPointName = arPointCardView.findViewById<TextView>(R.id.tv_title)
 
-                    val icon = ResourcesCompat.getDrawable(
-                        context.resources,
-                        this.icon,
-                        null
-                    )
+                        // Set the AR point icon and name
+                        arPointIcon.setImageResource(R.drawable.place)
+                        arPointName.text = arPoints[i].name
 
-                    icon?.let {
-                        it.setBounds(x.roundToInt() - pointSize,y.roundToInt() - pointSize,x.roundToInt() + pointSize,y.roundToInt() + pointSize)
-                        it.draw(canvas)
+                        // Measure the cardview to get its actual width and height
+                        arPointCardView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+                        val cardViewWidth = arPointCardView.measuredWidth
+                        val cardViewHeight = arPointCardView.measuredHeight
+
+                        // Set the click listener for the AR point layout
+                        arPointCardView.setOnClickListener {
+                            pointClickListener.onClick(arPoints[i])
+                        }
+
+                        // Add the AR point layout to the AROverlayView
+                        val parentView = parent as ViewGroup
+                        if (parentView != null) {
+                            parentView.addView(arPointCardView)
+                        }
+
+                        // Save the current AR point layout reference
+                        arPointLayouts.add(arPointCardView)
+
+                        // Update the AR point layout position
+                        val arPointCardViewLayoutParams = FrameLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                        )
+                        arPointCardViewLayoutParams.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+                        arPointCardView.layoutParams = arPointCardViewLayoutParams
+
+                        val marginInPixels = resources.getDimensionPixelSize(R.dimen.margin)
+                        arPointCardViewLayoutParams.setMargins(0, marginInPixels, 0, 0)
+
+                        arPointCardView.x = x
+                        arPointCardView.y = y
                     }
+
+                    // Update the AR point layout position
+                    val arPointLayout = arPointLayouts[i]
+                    arPointLayout.x = x
+                    arPointLayout.y = y
                 }
+            }
+
+            // Remove any extra AR point layouts if present
+            if (arPointLayouts.size > arPoints.size) {
+                for (i in arPoints.size until arPointLayouts.size) {
+                    val arPointLayout = arPointLayouts[i]
+                    val parentView = parent as ViewGroup
+                    parentView.removeView(arPointLayout)
+                }
+                arPointLayouts.removeAll { true }
             }
         }
     }
